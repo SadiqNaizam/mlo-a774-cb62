cce8d2600652ecab7846bc7bff0afa7f6644c014
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-24 11:07:38 +0530

    Files generated: CardHeader.tsx

---
+++ src/components/Login/CardHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CardHeader: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-card-foreground">
+        Welcome
+      </h1>
+    </div>
+  );
+};
+
+export default CardHeader;
